fdcb43e5aed414bc37e7522e32596312c820c730
r-garrido <rgarridodomenech@outlook.com>
2022-10-30 13:31:47 +0100

    cleanup

--- simulation.py
+++ simulation.py
@@ -22,11 +22,12 @@ while r > r_f:
     v += a * dt
     t += dt
 
-print('With a dt of ' + str(dt) + ' our simulation time in seconds is')
-print(t)
+print("dt: " + str(round(dt, 6)))
+print("simulation time: " + str(round(t, 6)))
 
-print('According to our full final equation, the following should be the time in seconds:')
-print(- math.sqrt(r_i / (2*gravitational_constant*(m_1 + m_2))) * (r_i * (math.atan(math.sqrt(r_f / delta_s)) - 0.5 * math.pi) - math.sqrt(r_f * delta_s)))
+equation_time = - math.sqrt(r_i / (2 * gravitational_constant * (m_1 + m_2))) * (
+            r_i * (math.atan(math.sqrt(r_f / delta_s)) - 0.5 * math.pi) - math.sqrt(r_f * delta_s))
+print("equation time: " + str(round(equation_time, 6)))
 
-print("And according to the Kepler equation, we get:")
-print(math.pi * math.sqrt(pow(r_i, 3) / (8 * gravitational_constant * (m_1 + m_2))))
+kepler_time = math.pi * math.sqrt(pow(r_i, 3) / (8 * gravitational_constant * (m_1 + m_2)))
+print("kepler time: " + str(round(kepler_time, 6)))
